d0e7b0bb0a87050625a6c02ff4fd1df793ea7967
Jonathan Raines <jr0278@bristol.ac.uk>
2024-09-25 14:43:16 +0000

    Add component group architecture and autograd simplifying components

--- moveable_morphable_components/components.py
+++ moveable_morphable_components/components.py
@@ -1,225 +1,138 @@
-from functools import partial
-from typing import Callable, NamedTuple
+from __future__ import annotations
 
-import jax
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Callable, NamedTuple
+
 import jax.numpy as jnp
 import numpy as np
-from numpy.typing import NDArray
+
+if TYPE_CHECKING:
+    from numpy.typing import NDArray
 
 
 class Point2D(NamedTuple):
-    x: float | jnp.ndarray
-    y: float | jnp.ndarray
+    """Point in 2D space."""
+
+    x: np.float64 | jnp.ndarray
+    y: np.float64 | jnp.ndarray
+
+
+@dataclass(frozen=True)
+class ComponentGroup:
+    """A collection of components that share the same topology description function and design variables."""
+
+    topology_description_function: Callable[[
+        NamedTuple], np.float64 | jnp.ndarray]
+    variable_initial: NDArray[np.float64]
+    variable_mins: NDArray[np.float64]
+    variable_maxes: NDArray[np.float64]
+    frozen_variables: list[int] | None = None
+
+    @property
+    def tdf(self) -> Callable[[NamedTuple], np.float64 | jnp.ndarray]:
+        """Allow abbreviated access to the topology description function."""
+        return self.topology_description_function
+
+    @property
+    def free_variable_col_indexes(self) -> NDArray:
+        """Indexes of non-frozen design variables."""
+        return np.setdiff1d(
+            np.arange(self.variable_initial.shape[1]), self.frozen_variables,
+        )
+
+    @property
+    def num_components(self) -> int:
+        """Number of components in the group."""
+        return self.variable_initial.shape[0]
+
+    @property
+    def num_design_variables(self) -> int:
+        """Number of free (non-frozen) design variables for the whole group."""
+        return self.variable_initial[:, self.free_variable_col_indexes].size
+
+    @property
+    def variable_initials_flattened(self) -> NDArray:
+        """Starting values of the design variables for each component.
+
+        Stacked into a flat array.
+        """
+        return self.variable_initial[:, self.free_variable_col_indexes].flatten()
+
+    @property
+    def bounds_flattened(self) -> tuple[NDArray[np.float64], NDArray[np.float64]]:
+        """Return a tuple containing the bounds for each design variable in the group.
+
+        E.g. a group of two circles defined by [Point2D(x,y), radius] could have bounds
+        as follows:
+
+        (
+            [Point2D(0.0, 0.0), 0.0, Point2D(0.0, 0.0), 0.0],
+            [Point2D(2.0, 1.0), 2.0, Point2D(2.0, 1.0), 2.0],
+        )
+        """
+        mins: NDArray[np.float64] = np.tile(
+            self.variable_mins[self.free_variable_col_indexes], self.num_components,
+        )
+        maxes: NDArray[np.float64] = np.tile(
+            self.variable_maxes[self.free_variable_col_indexes], self.num_components,
+        )
+        return mins, maxes
 
 
 class CircleSpec(NamedTuple):
+    """Design variables for a circle."""
+
     center: Point2D
     radius: float | jnp.ndarray
 
 
 def circle(
     point: Point2D,
-) -> tuple[
-    Callable[[CircleSpec], float | jnp.ndarray],
-    Callable[[CircleSpec], float | jnp.ndarray],
-]:
+) -> Callable[[CircleSpec], np.float64 | jnp.ndarray]:
     """Create a topological description function for a circle."""
 
-    def φ(spec: CircleSpec) -> float | jnp.ndarray:
+    def tdf(spec: CircleSpec) -> np.float64 | jnp.ndarray:
         """Topological Description Function for a circle."""
-        return (
-            spec.radius**2
-            - (point.x - spec.center.x) ** 2
-            - (point.y - spec.center.y) ** 2
-        )
+        center = Point2D(*spec[:2])
+        radius = spec[2]
+        return radius**2 - (point.x - center.x) ** 2 - (point.y - center.y) ** 2
 
-    J = jax.jacobian(φ)
-
-    return φ, J
+    return tdf
 
 
 class BeamSpec(NamedTuple):
+    """Design Variables for a beam."""
+
     center: Point2D
     angle: float | jnp.ndarray
     length: float | jnp.ndarray
     thickness: float | jnp.ndarray
 
 
-def uniform_beam(point: Point2D) -> tuple[Callable, Callable]:
-    def φ(spec: BeamSpec) -> float | jnp.ndarray:
-        """Topological Description Function for a circle."""
-        center, angle, length, thickness = spec
+def uniform_beam(
+    point: Point2D,
+) -> Callable[[BeamSpec], np.float64 | jnp.ndarray]:
+    """Create Topological Description Function for a beam with a uniform width."""
+
+    def tdf(spec: BeamSpec) -> np.float64 | jnp.ndarray:
+        """Topological Description Function for a beam with a uniform width."""
+        center = Point2D(*spec[:2])
+        angle, length, thickness = spec[2:]
         # because the matrix gets flipped top to bottom
         rotation_matrix: NDArray = jnp.array(
             [
                 [jnp.cos(angle), jnp.sin(angle)],
                 [-jnp.sin(angle), jnp.cos(angle)],
-            ]
+            ],
         )
         # Local coordinates
         _x, _y = rotation_matrix @ jnp.stack(
             [
                 (point.x - center.x),
                 (point.y - center.y),
-            ]
+            ],
         )
 
         return -jnp.maximum(jnp.abs(_x) - length / 2, jnp.abs(_y) - thickness / 2)
 
-    J = jax.jacobian(φ)
-
-    return φ, J
-
-
-class Component:
-    def __call__(self, points: tuple[NDArray[float] | float]) -> NDArray:
-        return self.φ(points[0], points[1])
-
-    @property
-    def design_variables(self) -> list[float]:
-        return [float(v) for v in vars(self).values()]
-
-    @property
-    def num_design_variables(self) -> int:
-        return len(self.design_variables)
-
-    def φ(self, x: NDArray[float] | float, y: NDArray[float] | float) -> float:
-        return jnp.vectorize(self._φ)(*self.design_variables, x, y)
-
-    def _φ(x: float, y: float) -> float:
-        raise NotImplementedError
-
-    def φ_grad(self, point: tuple[float] | NDArray[float]) -> float | NDArray[float]:
-        design_var_args = np.arange(self.num_design_variables)
-        f = partial(
-            jax.grad(self._φ, argnums=design_var_args), *list(self.design_variables)
-        )
-
-        return jnp.vectorize(f)(point[0], point[1])
-
-
-class Circle(Component):
-    def __init__(self, center: Point2D, radius: float):
-        self.x = center.x
-        self.y = center.y
-        self.radius = radius
-
-    def __call__(self, x: NDArray, y: NDArray) -> NDArray:
-        return self.φ(x, y)
-
-    def φ(self, x: NDArray, y: NDArray) -> float:
-        return self._φ(*self.design_variables, x, y)
-
-    def _φ(
-        self, center_x: float, center_y: float, radius: float, x: NDArray, y: NDArray
-    ) -> NDArray:
-        φ: float = radius**2 - (x - center_x) ** 2 - (y - center_y) ** 2
-        return φ
-
-
-class UniformBeam(Component):
-    def __init__(self, center: Point2D, angle: float, length: float, thickness: float):
-        """A beam of uniform thickness.
-
-        Args:
-            center (Point2D): Center of the beam
-            angle (float): rotation relative to x-axis in radians
-            length (float): length of the beam
-            thickness (float): thickness of the beam
-        """
-        self.x = center.x
-        self.y = center.y
-        self.angle = angle
-        self.length = length
-        self.thickness = thickness
-
-    def _φ(
-        self,
-        center_x: float,
-        center_y: float,
-        angle: float,
-        length: float,
-        thickness: float,
-        x: float,
-        y: float,
-    ) -> float:
-        # -angle because the matrix gets flipped top to bottom
-        rotation_matrix: NDArray = jnp.array(
-            [
-                [jnp.cos(angle), jnp.sin(angle)],
-                [-jnp.sin(angle), jnp.cos(angle)],
-            ]
-        )
-        # Local coordinates
-        _x, _y = rotation_matrix @ jnp.stack(
-            [
-                (x - center_x),
-                (y - center_y),
-            ]
-        )
-
-        φ: NDArray = -jnp.maximum(jnp.abs(_x) - length / 2, jnp.abs(_y) - thickness / 2)
-        return φ
-
-    def __repr__(self) -> str:
-        return f"UniformBeam(center=({self.x}, {self.y}), angle={float(np.rad2deg(self.angle)):2f}°, length={self.length:2f}, thickness={self.thickness:2f})"
-
-    def __str__(self) -> str:
-        return self.__repr__()
-
-
-class UniformBeamFixedThickness(Component):
-    def __init__(self, center: Point2D, angle: float, length: float, thickness: float):
-        """A beam of uniform thickness.
-
-        Args:
-            center (Point2D): Center of the beam
-            angle (float): rotation relative to x-axis in radians
-            length (float): length of the beam
-            thickness (float): thickness of the beam
-        """
-        self.x = center.x
-        self.y = center.y
-        self.angle = angle
-        self.length = length
-        self.thickness = thickness
-
-    # Override the design variables to exclude the thickness
-    @Component.design_variables.getter
-    def design_variables(self) -> list[float]:
-        return [float(v) for v in [self.x, self.y, self.angle, self.length]]
-
-    def _φ(
-        self,
-        center_x: float,
-        center_y: float,
-        angle: float,
-        length: float,
-        x: float,
-        y: float,
-    ) -> float:
-        # -angle because the matrix gets flipped top to bottom
-        rotation_matrix: NDArray = jnp.array(
-            [
-                [jnp.cos(-angle), -jnp.sin(-angle)],
-                [jnp.sin(-angle), jnp.cos(-angle)],
-            ]
-        )
-        # Local coordinates
-        _x, _y = rotation_matrix @ jnp.stack(
-            [
-                (x - center_x),
-                (y - center_y),
-            ]
-        )
-
-        φ: NDArray = -jnp.maximum(
-            jnp.abs(_x) - length / 2, jnp.abs(_y) - self.thickness / 2
-        )
-        return φ
-
-    def __repr__(self) -> str:
-        return f"UniformBeamFixedThickness(center=({self.x}, {self.y}), angle={float(np.rad2deg(self.angle)):2f}°, length={self.length:2f}, thickness={self.thickness:2f})"
-
-    def __str__(self) -> str:
-        return self.__repr__()
+    return tdf
