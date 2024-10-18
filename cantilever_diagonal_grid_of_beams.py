Bugfix gradients in main loop

cantilever_diagonal_grid_of_beams.py
@@ -1,10 +1,23 @@
+"""Demo the MMC package being used.
+
+Using the common cantilever beam example with a diagonal grid of beams.
+Defaults to 100 iterations, but can be changed with the --iterations flag.
+
+Example:
+    - python cantilever_diagonal_grid_of_beams.py
+    - python cantilever_diagonal_grid_of_beams.py --iterations 300
+
+"""
+
+import argparse
+
 import numpy as np
 
 from moveable_morphable_components import components, layout, main, plot
 from moveable_morphable_components.domain import Domain2D, Point2D
 
 
-def cantilever() -> None:
+def cantilever(max_iterations: int) -> None:
     # Create the domain
     domain: Domain2D = Domain2D(dimensions=(2.0, 1.0), element_shape=(80, 40))
 
@@ -26,16 +39,18 @@ def cantilever() -> None:
         topology_description_function=components.uniform_beam(
             Point2D(*domain.node_coordinates),
         ),
-        variable_initial=layout.grid_of_uniform_beams(
-            2, 2, domain.dimensions, 0.1),
+        variable_initial=layout.grid_of_uniform_beams(2, 2, domain.dimensions, 0.1),
         variable_mins=np.array([0.0, 0.0, -np.pi / 2, 0.1, 0.05]),
         variable_maxes=np.array(
             [domain.dims.x, domain.dims.y, np.pi / 2, 2.0, 0.2],
         ),
     )
 
+    # Run the MMC optimization.
+    # Note: expects component_list as a list of component groups so beams is passed
+    # in a list.
     design_variables, objective, constraint = main.main(
-        max_iterations=100,
+        max_iterations=max_iterations,
         domain=domain,
         boundary_conditions=boundary_conditions,
         volume_fraction_limit=0.4,
@@ -46,8 +61,23 @@ def cantilever() -> None:
         design_variable_history=design_variables,
         component_groups=[beams],
         domain=domain,
-        filename="cantilever_example")
+        duration=design_variables.shape[0] * 100,
+        filename="cantilever_example",
+    )
+
+    plot.objective_and_constraint(
+        objective,
+        constraint,
+    ).show()
 
 
 if __name__ == "__main__":
-    cantilever()
+    parser = argparse.ArgumentParser(description="Run cantilever beam optimization.")
+    parser.add_argument(
+        "--iterations",
+        type=int,
+        default=100,
+        help="Number of iterations for the optimization process.",
+    )
+    args = parser.parse_args()
+    cantilever(args.iterations)

moveable_morphable_components/components.py
@@ -1,7 +1,8 @@
 from __future__ import annotations
 
+from collections.abc import Callable
 from dataclasses import dataclass
-from typing import TYPE_CHECKING, Callable, NamedTuple
+from typing import TYPE_CHECKING, NamedTuple
 
 import jax.numpy as jnp
 import numpy as np
@@ -16,8 +17,7 @@ if TYPE_CHECKING:
 class ComponentGroup:
     """A collection of components that share the same topology description function and design variables."""
 
-    topology_description_function: Callable[[
-        NamedTuple], np.float64 | jnp.ndarray]
+    topology_description_function: Callable[[NamedTuple], np.float64 | jnp.ndarray]
     variable_initial: NDArray[np.float64]
     variable_mins: NDArray[np.float64]
     variable_maxes: NDArray[np.float64]
@@ -32,7 +32,8 @@ class ComponentGroup:
     def free_variable_col_indexes(self) -> NDArray:
         """Indexes of non-frozen design variables."""
         return np.setdiff1d(
-            np.arange(self.variable_initial.shape[1]), self.frozen_variables,
+            np.arange(self.variable_initial.shape[1]),
+            self.frozen_variables,
         )
 
     @property
@@ -66,10 +67,12 @@ class ComponentGroup:
         The first array is the min for each variable, and the second array is the max.
         """
         mins: NDArray[np.float64] = np.tile(
-            self.variable_mins[self.free_variable_col_indexes], self.num_components,
+            self.variable_mins[self.free_variable_col_indexes],
+            self.num_components,
         )
         maxes: NDArray[np.float64] = np.tile(
-            self.variable_maxes[self.free_variable_col_indexes], self.num_components,
+            self.variable_maxes[self.free_variable_col_indexes],
+            self.num_components,
         )
         return mins, maxes
 

moveable_morphable_components/layout.py
@@ -9,7 +9,10 @@ from moveable_morphable_components import components
 
 
 def grid_of_uniform_beams(
-    n_x: int, n_y: int, dimensions: tuple[float, float], thickness: float,
+    n_x: int,
+    n_y: int,
+    dimensions: tuple[float, float],
+    thickness: float,
 ) -> jnp.ndarray:
     """Initialise a grid of crossed Uniform Beams in the domain.
 
@@ -28,103 +31,96 @@ def grid_of_uniform_beams(
 
     # Generate the center coordinates of regions
     x_coords: NDArray = np.linspace(
-        region_size[0] / 2, dimensions[0] - region_size[0] / 2, n_x,
+        region_size[0] / 2,
+        dimensions[0] - region_size[0] / 2,
+        n_x,
     )
     y_coords: NDArray = np.linspace(
-        region_size[1] / 2, dimensions[1] - region_size[1] / 2, n_y,
+        region_size[1] / 2,
+        dimensions[1] - region_size[1] / 2,
+        n_y,
     )
 
     # Angle and length to put the components across the region diagonals
     angle: float = np.arctan2(region_size[1], region_size[0])
     length: float = np.linalg.norm(region_size)
 
-    for y, x in itertools.product(y_coords, x_coords):
-        for sign in [-1, 1]:
-            design_variables.append([x, y, sign * angle, length, thickness])
+    design_variables = [
+        [x, y, sign * angle, length, thickness]
+        for sign, y, x in itertools.product([-1, 1], y_coords, x_coords)
+    ]
 
     return jnp.array(design_variables)
 
 
 def grid_of_uniform_beams_of_fixed_thickness(
-    n_x: int, n_y: int, dimensions: tuple[float, float], thickness: list[float] | float,
+    n_x: int,
+    n_y: int,
+    dimensions: tuple[float, float],
+    thickness: list[float] | float,
 ) -> jnp.ndarray:
-    """Initialises a grid of crossed Uniform Beams in the domain
+    """Initialise a grid of crossed Uniform Beams in the domain.
 
-    Parameters
-    ----------
+    Parameters;
         n_x: int - The number of component pairs (crosses) in the x direction
         n_y: int - The number of component pairs (crosses) in the y direction
         dimensions: tuple[float, float] - The dimensions to fill with the grid
         thickness: float - The thickness of the beam components
 
-    Returns
-    -------
+    Returns:
         list[Component] - The list of components
 
     """
     if isinstance(thickness, float):
         thickness = [thickness] * n_x * n_y * 2
 
-    assert (
-        len(thickness) == n_x * n_y * 2
-    ), "thickness must be the correct length (nx * ny * 2)"
+    if len(thickness) != n_x * n_y * 2:
+        msg = f"""Thickness list must be the correct length (nx * ny * 2),
+          got {len(thickness)}"""
+        raise ValueError(
+            msg,
+        )
 
     # Work out the size of the regions each crossed pair will occupy
     region_size: NDArray = dimensions / (n_x, n_y)
 
     # Generate the center coordinates of regions
     x_coords: NDArray = np.linspace(
-        region_size[0] / 2, dimensions[0] - region_size[0] / 2, n_x,
+        region_size[0] / 2,
+        dimensions[0] - region_size[0] / 2,
+        n_x,
     )
     y_coords: NDArray = np.linspace(
-        region_size[1] / 2, dimensions[1] - region_size[1] / 2, n_y,
+        region_size[1] / 2,
+        dimensions[1] - region_size[1] / 2,
+        n_y,
     )
 
     # Angle and length to put the components across the region diagonals
     angle: float = np.arctan2(region_size[1], region_size[0])
     length: float = np.linalg.norm(region_size)
 
-    component_list: list[components.Component] = []
-    i = 0
-    for y, x in itertools.product(y_coords, x_coords):
-        for sign in [-1, 1]:
-            component_list.append(
-                components.UniformBeamFixedThickness(
-                    center=components.Point2D(x, y),
-                    angle=sign * angle,
-                    length=length,
-                    thickness=thickness[i],
-                ),
-            )
-            i += 1
+    design_variables = [
+        [x, y, sign * angle, length, thickness[i]]
+        for i, (sign, y, x) in enumerate(itertools.product([-1, 1], y_coords, x_coords))
+    ]
 
-    return component_list
+    return jnp.array(design_variables)
 
 
-def random_beams(
-    min: NDArray,
-    max: NDArray,
+def random(
+    low: NDArray,
+    high: NDArray,
     n: int,
     np_random: np.random.Generator | None,
 ) -> jnp.ndarray:
-    component_list = []
-
+    """Randomly place n components in the domain."""
     if np_random is None:
         np_random = np.random.default_rng()
 
-    design_vars = np.random.uniform(low=min, high=max, size=(n, 5))
-    for i in range(n):
-        x, y, angle, length, thickness = design_vars[i]
-        component_list.append(
-            components.UniformBeam(
-                center=components.Point2D(x, y),
-                angle=angle,
-                length=length,
-                thickness=thickness,
-            ),
-        )
+    design_vars = np_random.uniform(low=low, high=high, size=(n, low.size))
 
-    return component_list
+    return jnp.array(design_vars)
 
 
 def fixed_thickness_beam_from_df(df: pd.DataFrame) -> jnp.ndarray:

moveable_morphable_components/main.py
@@ -1,4 +1,5 @@
 """The main loop of the MMC method."""
+
 from __future__ import annotations
 
 import itertools
@@ -23,14 +24,13 @@ if TYPE_CHECKING:
     from moveable_morphable_components.components import ComponentGroup
     from moveable_morphable_components.domain import Domain2D
 
-HEAVISIDE_MIN_VALUE: float = 1e-3  # 1e-9  # Heaviside minimum value
+HEAVISIDE_MIN_VALUE: float = 1e-6  # 1e-9  # Heaviside minimum value
 # 10  # Size of transition region for the smoothed Heaviside function
 HEAVISIDE_TRANSITION_WIDTH: float = 0.01
 POISSON_RATIO: float = 0.3  # Poisson's ratio
 THICKNESS: float = 1.0  # Thickness
 
 YOUNGS_MODULUS: float = 1e1  # 1e7  # Young's modulus N/mm^2
-FORCE_MAGNITUDE: float = 1  # N
 
 SCALE: float = 1.0
 NUM_CONSTRAINTS: int = 1  # Volume fraction constraint
@@ -52,7 +52,8 @@ def main(
     """Do MMC method."""
     # Make a mask for the free dofs
     free_dofs: NDArray[np.uint] = np.setdiff1d(
-        np.arange(domain.num_dofs), boundary_conditions["fixed_dof_ids"],
+        np.arange(domain.num_dofs),
+        boundary_conditions["fixed_dof_ids"],
     )
 
     # Load the beam on the RHS half way up
@@ -67,7 +68,10 @@ def main(
 
     # Define the element stiffness matrix
     element_stiffness: NDArray[np.float64] = finite_element.element_stiffness_matrix(
-        YOUNGS_MODULUS, POISSON_RATIO, domain.element_size, THICKNESS,
+        YOUNGS_MODULUS,
+        POISSON_RATIO,
+        domain.element_size,
+        THICKNESS,
     )
 
     # Combine the initials, mins, and maxes for the design variables from each group
@@ -93,8 +97,7 @@ def main(
     constraint_history: list[float] = []
 
     # Combine the level set functions from the components to form a global one
-    group_tdfs: list[Callable] = [compose_tdfs(
-        tdf=cg.tdf) for cg in component_list]
+    group_tdfs: list[Callable] = [compose_tdfs(tdf=cg.tdf) for cg in component_list]
 
     structure_tdf: Callable = compose_structure_tdf(group_tdfs, component_list)
 
@@ -104,14 +107,14 @@ def main(
         transition_width=HEAVISIDE_TRANSITION_WIDTH,
         minimum_value=HEAVISIDE_MIN_VALUE,
     )
+    # heaviside_structure: Callable = make_leaky_relu(tdf=structure_tdf)
 
     # Optimisation loop
     for iteration in tqdm.trange(max_iterations):
         # Save the design variables
         design_variables_history[iteration] = design_variables
         # Calculate the density of the elements
-        node_densities: NDArray[np.float64] = heaviside_structure(
-            design_variables)
+        node_densities: NDArray[np.float64] = heaviside_structure(design_variables)
         element_densities: NDArray[np.float64] = domain.average_node_values_to_element(
             node_densities,
         )
@@ -124,16 +127,19 @@ def main(
         )
 
         # Reduce the stiffness matrix to the free dofs
-        stiffness_free: scipy.sparse.csc_matrix = stiffness[free_dofs,
-                                                            :][:, free_dofs]
+        stiffness_free: scipy.sparse.csc_matrix = stiffness[free_dofs, :][:, free_dofs]
 
         # Solve the system
         displacements: NDArray[np.float64] = np.zeros(domain.num_dofs)
         displacements[free_dofs] = scipy.sparse.linalg.spsolve(
-            stiffness_free, forces[free_dofs])
+            stiffness_free,
+            forces[free_dofs],
+        )
 
         # Calculate the Energy of the Elements
-        element_displacements: NDArray[np.float64] = displacements[domain.element_dof_ids]
+        element_displacements: NDArray[np.float64] = displacements[
+            domain.element_dof_ids
+        ]
         element_energy: NDArray[np.float64] = np.sum(
             (element_displacements @ element_stiffness) * element_displacements,
             axis=1,
@@ -142,35 +148,29 @@ def main(
         node_energy: NDArray[np.float64] = domain.element_value_to_nodes(
             element_energy,
         ).reshape((-1, 1), order="F")
+        log_node_energy = np.log(node_energy + 1 + 1e-9)
+        # plot_values(log_node_energy, (81, 41), title="Log Node Energy").show()
 
         # Sensitivity Analysis
 
         # Objective and derivative
         objective: NDArray[np.float64] = forces.T @ displacements * SCALE
         objective_history.append(objective[0])
-        grads = jax.jacobian(
-            heaviside_structure)(design_variables)
+        grads = jax.jacobian(heaviside_structure)(design_variables)
         d_objective_d_design_vars = jnp.nansum(
-            -node_energy * grads, axis=0,
+            -log_node_energy * grads,
+            axis=0,
         )
 
         # Volume fraction constraint and derivative
         volume_fraction_constraint: float = (
-            jnp.sum(
-                make_heaviside(
-                    tdf=structure_tdf,
-                    transition_width=HEAVISIDE_TRANSITION_WIDTH,
-                    minimum_value=0)(
-                        design_variables,
-                )
-                * domain.node_volumes.reshape((-1, 1), order="F"),
-            )
-            / jnp.sum(domain.node_volumes)
-            - volume_fraction_limit
+            jnp.mean(node_densities) - volume_fraction_limit
         )
-        contraint_grads: NDArray[np.float64] = jnp.nansum(
-            domain.node_volumes.reshape(
-                (-1, 1), order="F") * grads,
+
+        # TODO(JonathanRaines): The solution seems independent
+        # of the constraint gradient sign. Flipping it does nothing...
+        constraint_grads: NDArray[np.float64] = jnp.nansum(
+            domain.node_volumes.reshape((-1, 1), order="F") * grads,
             axis=0,
         )
 
@@ -185,13 +185,17 @@ def main(
             xmin=np.expand_dims(design_variables_min, axis=1),
             xmax=np.expand_dims(design_variables_max, axis=1),
             xold1=np.expand_dims(
-                design_variables_history[max(0, iteration - 1)], axis=1),
+                design_variables_history[max(0, iteration - 1)],
+                axis=1,
+            ),
             xold2=np.expand_dims(
-                design_variables_history[max(0, iteration - 2)], axis=1),
+                design_variables_history[max(0, iteration - 2)],
+                axis=1,
+            ),
             f0val=objective,
             df0dx=np.expand_dims(d_objective_d_design_vars, 1),
             fval=np.expand_dims(volume_fraction_constraint, axis=0),
-            dfdx=np.expand_dims(contraint_grads, 0),
+            dfdx=np.expand_dims(constraint_grads, 0),
             low=low,
             upp=upp,
             a0=A0,
@@ -250,16 +254,32 @@ def compose_tdfs(
 
         """
         return jnp.max(jax.vmap(tdf)(design_variables), axis=0)
+        # K-S method as per original paper
+        return (
+            jnp.log(jnp.sum(jnp.exp(jax.vmap(tdf)(design_variables) * 100), axis=0))
+            / 100
+        )
 
     return group_tdf
 
 
 def compose_structure_tdf(
-    group_tdfs: list[Callable], component_list: list[ComponentGroup],
+    group_tdfs: list[Callable],
+    component_list: list[ComponentGroup],
 ) -> Callable[[NDArray[np.float64]], NDArray[np.float64]]:
-    """Return a function that unravels the design variables and calculates the combined TDF for the structure."""
+    """Compose component Topology Description Functions into a structure TDF.
+
+    Return a function that unravels the design variables and calculates the combined TDF
+        for the structure.
+    """
 
     def structure_tdf(design_variables: NDArray[np.float64]) -> NDArray[np.float64]:
+        """Evaluate the Topology Description Function for the structure.
+
+        Given some design variable, calculate the value of the topology description
+        function at each point in the domain. Points are set when the component
+        group TDF is created.
+        """
         group_design_variable_counts = jnp.array(
             [cg.num_design_variables for cg in component_list],
         )
@@ -267,24 +287,33 @@ def compose_structure_tdf(
             [cg.free_variable_col_indexes.size for cg in component_list],
         )
         if design_variables.size != jnp.sum(group_design_variable_counts):
-            msg = "design_variables has the wrong number of elements for the component groups provided"
+            msg = """Design_variables has the wrong number of elements for the component
+                groups provided"""
             raise ValueError(msg)
 
         group_design_variables_flat: list[NDArray] = jnp.split(
-            design_variables, jnp.cumsum(group_design_variable_counts),
+            design_variables,
+            jnp.cumsum(group_design_variable_counts)[:-1],
         )
         group_design_variables = [
             design_variables.reshape(-1, n)
             for design_variables, n in zip(
-                group_design_variables_flat, group_design_variables_per_component,
+                group_design_variables_flat,
+                group_design_variables_per_component,
+                strict=True,
             )
         ]
 
         group_tdf_values = [
-            tdf(dv) for tdf, dv in zip(group_tdfs, group_design_variables)
+            tdf(dv) for tdf, dv in zip(group_tdfs, group_design_variables, strict=True)
         ]
 
         return jnp.max(jnp.array(group_tdf_values), axis=0)
+
+        # K-S method as per original paper
+        return (
+            jnp.log(jnp.sum(jnp.exp(jnp.array(group_tdf_values) * 100), axis=0)) / 100
+        )
 
     return structure_tdf
 
@@ -299,7 +328,8 @@ def is_converged(
     """Check if the optimisation has converged."""
     if iteration > window_size and constraint_value < 0:
         smoothed_objective_change: NDArray = moving_average(
-            objective_history, window_size,
+            objective_history,
+            window_size,
         )
         smoothed_objective_deltas: NDArray = np.diff(smoothed_objective_change)
         return bool(np.all(np.abs(smoothed_objective_deltas) < objective_tolerance))
@@ -310,7 +340,7 @@ def moving_average(values, n):
     """Calculate a rolling average for values with window size n."""
     ret: NDArray = np.cumsum(values, dtype=float)
     ret[n:] = ret[n:] - ret[:-n]
-    return ret[n - 1:] / n
+    return ret[n - 1 :] / n
 
 
 def make_heaviside(
@@ -350,11 +380,29 @@ def make_heaviside(
     return smooth_heaviside
 
 
-def plot_values(values: NDArray, domain_shape: tuple[int, int]) -> go.Figure:
+def make_leaky_relu(
+    tdf: Callable[[NDArray[np.float64]], NDArray[np.float64]],
+) -> Callable[[NDArray[np.float64]], NDArray[np.float64]]:
+    def leaky_relu(design_variables: NDArray[np.float64]) -> NDArray[np.float64]:
+        x = tdf(design_variables)
+        negative_slope = 1
+        return jnp.where(x > 0, 1 + x * negative_slope, x * negative_slope)
+
+    return leaky_relu
+
+
+def plot_values(
+    values: NDArray,
+    domain_shape: tuple[int, int],
+    title: str = "",
+) -> go.Figure:
     """Plot values for debugging."""
     return go.Figure(
         data=go.Contour(
             z=values.reshape(domain_shape, order="F").T,
+        ),
+        layout=go.Layout(
+            title=title,
         ),
     )
 
@@ -371,8 +419,7 @@ def connected_components(
 
     """
     num_components = singed_distance_functions.shape[0]
-    connectivity_matrix = np.zeros(
-        (num_components, num_components), dtype=bool)
+    connectivity_matrix = np.zeros((num_components, num_components), dtype=bool)
     for component_1, component_2 in itertools.combinations(range(num_components), 2):
         sdf_1 = singed_distance_functions[component_1, :, :]
         sdf_2 = singed_distance_functions[component_2, :, :]

moveable_morphable_components/plot.py
@@ -9,6 +9,7 @@ import plotly.graph_objects as go
 from PIL import Image
 from plotly.express.colors import qualitative, sample_colorscale
 from plotly.subplots import make_subplots
+from tqdm import tqdm
 
 if TYPE_CHECKING:
     from numpy.typing import NDArray
@@ -20,37 +21,74 @@ COLOURS = qualitative.Pastel[:2]
 TRANSPARENT = "rgba(0,0,0,0)"
 
 
-# def component_image(component_list, coords, dimensions) -> go.Figure:
-#     fig = go.Figure()
+def component_image(
+    component_groups: list[components.ComponentGroup],
+    design_variables: NDArray,
+    domain: Domain2D,
+) -> go.Figure:
+    num_components: int = sum([cg.num_design_variables for cg in component_groups])
+    contour_settings = {"start": 0, "end": 1, "size": 2}
+    colour_scales = [
+        [[0, TRANSPARENT], [1, c]]
+        for c in sample_colorscale(qualitative.Pastel, num_components)
+    ]
+    fig = go.Figure()
 
-#     tdf_values = evaluate_topology_description_functions(component_list, coords)
-#     contour_settings = {"start": 0, "end": 1, "size": 2}
-#     colour_scales = [[[0, TRANSPARENT], [1, c]] for c in COLOURS]
+    # Separate the design variables, reshape them into an array than is
+    # the correct number of design variables wide, then evaluate them
+    # at all the nodes in the domain.
+    group_design_var_count = [cg.num_design_variables for cg in component_groups]
+    design_vars_per_component = [
+        len(cg.free_variable_col_indexes) for cg in component_groups
+    ]
+    grouped_design_vars = np.split(
+        design_variables,
+        np.cumsum(group_design_var_count),
+        axis=0,
+    )
+    grouped_design_vars = [
+        gdv.reshape(-1, n)
+        for gdv, n in zip(
+            grouped_design_vars,
+            design_vars_per_component,
+            strict=False,
+        )
+    ]
+    n_x, n_y = domain.node_shape
+    sdfs = []
+    for i, group in enumerate(component_groups):
+        sdfs.extend(
+            [
+                group.tdf(design_vars).reshape(n_x, n_y, order="F")
+                for design_vars in grouped_design_vars[i]
+            ],
+        )
 
-#     traces = [
-#         go.Contour(
-#             z=sdf.T,
-#             colorscale=colour_scales[i % len(colour_scales)],
-#             contours=contour_settings,
-#             line_smoothing=0,
-#             showscale=False,
-#             showlegend=False,
-#             x=np.linspace(0, dimensions[0], coords[0].shape[0]),
-#             y=np.linspace(0, dimensions[1], coords[0].shape[1]),
-#         )
-#         for i, sdf in enumerate(tdf_values)
-#     ]
+    traces = [
+        go.Contour(
+            z=sdf.T,
+            colorscale=colour_scales[i % len(colour_scales)],
+            contours=contour_settings,
+            line_smoothing=0,
+            showscale=False,
+            showlegend=False,
+            x=np.linspace(0, domain.dims.x, n_x),
+            y=np.linspace(0, domain.dims.y, n_y),
+        )
+        for i, sdf in enumerate(sdfs)
+    ]
 
-#     fig.add_traces(traces)
-#     fig.update_layout(
-#         dict(
-#             template="simple_white",
-#             plot_bgcolor=TRANSPARENT,
-#             paper_bgcolor=TRANSPARENT,
-#         ),
-#     )
+    fig.add_traces(traces)
+    fig.update_layout(
+        {
+            "template": "simple_white",
+            # TODO(JonathanRaines): PIL seems to layer images so can't have transparent background
+            # plot_bgcolor=TRANSPARENT,
+            # paper_bgcolor=TRANSPARENT,
+        },
+    )
 
-#     return fig
+    return fig
 
 
 def save_component_animation(
@@ -60,65 +98,11 @@ def save_component_animation(
     duration: int = 5_000,
     filename: str = "mmc",
 ) -> None:
-    num_components: int = sum(
-        [cg.num_design_variables for cg in component_groups])
     frames: list[Image.Image] = []
-    contour_settings = {"start": 0, "end": 1, "size": 2}
-    colour_scales = [
-        [[0, TRANSPARENT], [1, c]]
-        for c in sample_colorscale(qualitative.Pastel, num_components)
-    ]
-
-    # Get the number of nodes in each axis.
-    n_x, n_y = domain.node_shape
 
     # For each timestep
-    for global_design_vars in design_variable_history:
-        fig = go.Figure()
-
-        # Separate the design variables, reshape them into an array than is
-        # the correct number of design variables wide, then evaluate them
-        # at all the nodes in the domain.
-        group_design_var_count = [
-            cg.num_design_variables for cg in component_groups]
-        design_vars_per_component = [
-            len(cg.free_variable_col_indexes) for cg in component_groups
-        ]
-        grouped_design_vars = np.split(
-            global_design_vars, np.cumsum(group_design_var_count), axis=0,
-        )
-        grouped_design_vars = [
-            gdv.reshape(-1, n)
-            for gdv, n in zip(grouped_design_vars, design_vars_per_component)
-        ]
-        sdfs = []
-        for i, group in enumerate(component_groups):
-            sdfs.extend([group.tdf(design_vars).reshape(n_x, n_y, order="F")
-                        for design_vars in grouped_design_vars[i]])
-
-        traces = [
-            go.Contour(
-                z=sdf.T,
-                colorscale=colour_scales[i % len(colour_scales)],
-                contours=contour_settings,
-                line_smoothing=0,
-                showscale=False,
-                showlegend=False,
-                x=np.linspace(0, domain.dims.x, n_x),
-                y=np.linspace(0, domain.dims.y, n_y),
-            )
-            for i, sdf in enumerate(sdfs)
-        ]
-
-        fig.add_traces(traces)
-        fig.update_layout(
-            {
-                "template": "simple_white",
-                # TODO(JonathanRaines): PIL seems to layer images so can't have transparent background
-                # plot_bgcolor=TRANSPARENT,
-                # paper_bgcolor=TRANSPARENT,
-            },
-        )
+    for global_design_vars in tqdm(design_variable_history, desc="Creating animation"):
+        fig = component_image(component_groups, global_design_vars, domain)
 
         frame = fig.to_image(format="png")
 
@@ -239,7 +223,10 @@ def objective_and_constraint(objective, constraint) -> go.Figure:
     obj_fig = make_subplots(specs=[[{"secondary_y": True}]])
     obj_fig.add_trace(
         go.Scatter(
-            x=np.arange(len(constraint)), y=objective, mode="lines", name="Objective",
+            x=np.arange(len(constraint)),
+            y=objective,
+            mode="lines",
+            name="Objective",
         ),
         secondary_y=False,
     )